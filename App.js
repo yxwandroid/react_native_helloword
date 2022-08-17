3fde5a44bc0365f073891b233d26aae345fe2a20
yxwandroid <yxw_android@163.com>
2022-08-17 11:41:18 +0800

    解决 bug

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
-import DetailsScreen from './src/helloword/DetailsScreen';
 import FlexBoxScreen from './src/helloword/FlexBoxScreen';
 import HomeScreen from './src/HomeScreen';
-import DemoScreen from './src/helloword/DemoScreen';
 const Stack = createStackNavigator();
 
 function App() {
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName="Home">
+            <Stack.Navigator initialRouteName="FlexBoxScreen">
                 <Stack.Screen name="Home" component={HomeScreen}/>
-                <Stack.Screen name="DetailsScreen" component={DetailsScreen}/>
                 <Stack.Screen name="FlexBoxScreen" component={FlexBoxScreen}/>
-                <Stack.Screen name="DemoScreen" component={DemoScreen}/>
             </Stack.Navigator>
         </NavigationContainer>
     );
